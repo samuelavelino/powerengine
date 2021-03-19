307e596d2e3c818cc5d36aa21455a19934bd20c7
Samuel Avelino <48601223+samuelavelino@users.noreply.github.com>
2021-03-19 18:46:54 -0300

    Update test.ps1

--- games/test/test.ps1
+++ games/test/test.ps1
@@ -1,1 +1,2 @@
-Write-Host "path: https://github.com/samuelavelino/powerengine/new/main/games/test/test.ps1" -ForegroundColor Green
+Write-Host "path: https://github.com/samuelavelino/powerengine/blob/main/games/test/test.ps1" -ForegroundColor Green
+Write-Host "raw: https://raw.githubusercontent.com/samuelavelino/powerengine/main/games/test/test.ps1" -ForegroundColor Yellow
