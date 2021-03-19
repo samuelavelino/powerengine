8e227a8b644841444af0d153cbeb882e4173afb8
Samuel Avelino <48601223+samuelavelino@users.noreply.github.com>
2021-03-19 19:03:43 -0300

    Update powerun.ps1

--- powerun.ps1
+++ powerun.ps1
@@ -1,9 +1,31 @@
-$txt = "PowerRun"
+$run=$args[0]
 
-function main
+try
 {
-    Write-Output $txt
+    switch ( $run )
+    {
+        ({$run -eq 'e' -or $run -eq 'error'})
+        {
+            $url = "https://raw.githubusercontent.com/samuelavelino/powerengine/main/games/error/error.ps1"
+            iex (New-Object System.Net.WebClient).DownloadString($url)
+            Break
+        }
+        ({$run -eq 't' -or $run -eq 'test'})
+        {
+            $url = "https://raw.githubusercontent.com/samuelavelino/powerengine/main/games/test/test.ps1"
+            iex (New-Object System.Net.WebClient).DownloadString($url)
+            Break
+        }
+        Default
+        {
+            Write-Output "Default: $run"
+        }
+    }
+}
+catch
+{
+    Write-Host "url: $url" -ForegroundColor Yellow
+    Write-Host "$_" -ForegroundColor Red
 }
 
-main
 Read-Host
